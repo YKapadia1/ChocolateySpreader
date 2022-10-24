035e76a0c4934faba5e8a8b5394e0c3000f7c69e
YKapadia <YKapadia@uclan.ac.uk>
2022-10-24 11:30:34 +0100

    Add extraction code + comments.

--- Form1.cs
+++ Form1.cs
@@ -89,17 +89,58 @@ namespace ChocolateySpreader
             }
             else
             {
+                //Check if 7-Zip is installed. This should work on both 32 and 64 bit systems.
+                //This will fail if the user has specified an alternate install location!
                 bool SevenZipInstalled = File.Exists("C:\\Program Files\\7-Zip\\7z.exe");
+                //If 7-Zip is not installed...
                 if (!SevenZipInstalled)
                 {
+                    //Let the user know
                     MessageBox.Show("7-Zip is not installed. Get it from 7-zip.org.", "Error",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //Open the 7-Zip website.
                     Process.Start("https://www.7-zip.org/");
                 }
                 else
                 {
-                    MessageBox.Show("Add code to extract the stuff!", "So close!",
-                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Process ExtractISO = new Process(); //Create a new process that we will start.
+                    //Set the file path to where 7-Zip is usually located.
+                    //This will break if the user has specified an alternate install location!
+                    ExtractISO.StartInfo.FileName =  "C:\\Program Files\\7-Zip\\7z.exe"; 
+                    ExtractISO.StartInfo.Arguments = " -o" + FolderPathBox.Text + " x " + ISOPathBox.Text; //Create the arguments necessary.
+                    ExtractISO.Start(); //Start the process.
+                    while (!ExtractISO.HasExited) //If the process has not yet exited...
+                    {
+                        //Do nothing.
+                    }
+                    switch (ExtractISO.ExitCode) //Check the exit code.
+                    {
+                        case 0: //If there was no error...
+                            MessageBox.Show("ISO extracted successfully!", "ChocolateySpreader",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            break;
+                        case 1: //If there were non-fatal errors/warnings...
+                            MessageBox.Show("ISO extracted successfully!", "ChocolateySpreader",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            break;
+                        case 2: //If there was a fatal error...
+                            MessageBox.Show("A fatal error occured when extracting the ISO.", "ChocolateySpreader",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        case 7: //If there was a command line error.
+                            MessageBox.Show("A command line error occured when extracting the ISO.", "ChocolateySpreader",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        case 8: //If we have ran out of memory...
+                            MessageBox.Show("There is not enough free memory available to extract the ISO.", "ChocolateySpreader",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        case 255: //If the user closed the window before it was completed or pressed CTRL+C...
+                            MessageBox.Show("The user cancelled the operation.", "ChocolateySpreader",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                    }
+
                 }
             }
         }
