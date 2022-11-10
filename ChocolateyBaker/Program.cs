52b85dcb6b1aaa45fe2b103f566eece422df6c52
YKapadia <YKapadia@uclan.ac.uk>
2022-11-10 19:35:49 +0000

    Added seperate project to aid in post-install operations

---
+++ ChocolateyBaker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ChocolateyBaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("This program requires arguments in order to function properly.");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+                Environment.Exit(2);
+            }
+        }
+    }
+
+}
+
