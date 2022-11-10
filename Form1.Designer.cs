07bd22211ca7d534f092c3a34053b1ab921758ea
Yamin Kapadia <YKapadia@uclan.ac.uk>
2022-11-10 14:56:37 +0000

    Added controls to select an ISO folder and package list, as well as a button to inject the auto install scripts.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,13 @@
             this.ExtractISOButton = new System.Windows.Forms.Button();
             this.OutputBox = new System.Windows.Forms.RichTextBox();
             this.OutputLabel = new System.Windows.Forms.Label();
+            this.ChocoSpreadButton = new System.Windows.Forms.Button();
+            this.PKGListButton = new System.Windows.Forms.Button();
+            this.ISOFolderButton = new System.Windows.Forms.Button();
+            this.ISOFolderBox = new System.Windows.Forms.TextBox();
+            this.PKGListBox = new System.Windows.Forms.TextBox();
+            this.PKGListLabel = new System.Windows.Forms.Label();
+            this.ISOFolderLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ISOPathLabel
@@ -62,16 +69,15 @@
             // FolderPathBox
             // 
             this.FolderPathBox.Location = new System.Drawing.Point(22, 82);
-            this.FolderPathBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FolderPathBox.Margin = new System.Windows.Forms.Padding(4);
             this.FolderPathBox.Name = "FolderPathBox";
             this.FolderPathBox.Size = new System.Drawing.Size(159, 22);
             this.FolderPathBox.TabIndex = 2;
-            this.FolderPathBox.TextChanged += new System.EventHandler(this.FolderPathBox_TextChanged);
             // 
             // ISOPathBox
             // 
             this.ISOPathBox.Location = new System.Drawing.Point(20, 36);
-            this.ISOPathBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ISOPathBox.Margin = new System.Windows.Forms.Padding(4);
             this.ISOPathBox.Name = "ISOPathBox";
             this.ISOPathBox.Size = new System.Drawing.Size(159, 22);
             this.ISOPathBox.TabIndex = 3;
@@ -79,7 +85,7 @@
             // ISOSelectButton
             // 
             this.ISOSelectButton.Location = new System.Drawing.Point(188, 35);
-            this.ISOSelectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ISOSelectButton.Margin = new System.Windows.Forms.Padding(4);
             this.ISOSelectButton.Name = "ISOSelectButton";
             this.ISOSelectButton.Size = new System.Drawing.Size(100, 24);
             this.ISOSelectButton.TabIndex = 4;
@@ -90,7 +96,7 @@
             // OutputFolderSelectButton
             // 
             this.OutputFolderSelectButton.Location = new System.Drawing.Point(189, 81);
-            this.OutputFolderSelectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OutputFolderSelectButton.Margin = new System.Windows.Forms.Padding(4);
             this.OutputFolderSelectButton.Name = "OutputFolderSelectButton";
             this.OutputFolderSelectButton.Size = new System.Drawing.Size(100, 24);
             this.OutputFolderSelectButton.TabIndex = 5;
@@ -101,7 +107,7 @@
             // ExtractISOButton
             // 
             this.ExtractISOButton.Location = new System.Drawing.Point(120, 130);
-            this.ExtractISOButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ExtractISOButton.Margin = new System.Windows.Forms.Padding(4);
             this.ExtractISOButton.Name = "ExtractISOButton";
             this.ExtractISOButton.Size = new System.Drawing.Size(100, 28);
             this.ExtractISOButton.TabIndex = 6;
@@ -112,7 +118,7 @@
             // OutputBox
             // 
             this.OutputBox.Location = new System.Drawing.Point(21, 363);
-            this.OutputBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OutputBox.Margin = new System.Windows.Forms.Padding(4);
             this.OutputBox.Name = "OutputBox";
             this.OutputBox.ReadOnly = true;
             this.OutputBox.Size = new System.Drawing.Size(385, 175);
@@ -129,13 +135,88 @@
             this.OutputLabel.TabIndex = 8;
             this.OutputLabel.Text = "Output";
             // 
+            // ChocoSpreadButton
+            // 
+            this.ChocoSpreadButton.Location = new System.Drawing.Point(99, 283);
+            this.ChocoSpreadButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ChocoSpreadButton.Name = "ChocoSpreadButton";
+            this.ChocoSpreadButton.Size = new System.Drawing.Size(149, 28);
+            this.ChocoSpreadButton.TabIndex = 15;
+            this.ChocoSpreadButton.Text = "Insert Scripts";
+            this.ChocoSpreadButton.UseVisualStyleBackColor = true;
+            this.ChocoSpreadButton.Click += new System.EventHandler(this.ChocoSpreadButton_Click);
+            // 
+            // PKGListButton
+            // 
+            this.PKGListButton.Location = new System.Drawing.Point(189, 233);
+            this.PKGListButton.Margin = new System.Windows.Forms.Padding(4);
+            this.PKGListButton.Name = "PKGListButton";
+            this.PKGListButton.Size = new System.Drawing.Size(100, 24);
+            this.PKGListButton.TabIndex = 14;
+            this.PKGListButton.Text = "Browse...";
+            this.PKGListButton.UseVisualStyleBackColor = true;
+            // 
+            // ISOFolderButton
+            // 
+            this.ISOFolderButton.Location = new System.Drawing.Point(187, 187);
+            this.ISOFolderButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ISOFolderButton.Name = "ISOFolderButton";
+            this.ISOFolderButton.Size = new System.Drawing.Size(100, 24);
+            this.ISOFolderButton.TabIndex = 13;
+            this.ISOFolderButton.Text = "Browse...";
+            this.ISOFolderButton.UseVisualStyleBackColor = true;
+            this.ISOFolderButton.Click += new System.EventHandler(this.ISOFolderButton_Click);
+            // 
+            // ISOFolderBox
+            // 
+            this.ISOFolderBox.Location = new System.Drawing.Point(20, 188);
+            this.ISOFolderBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ISOFolderBox.Name = "ISOFolderBox";
+            this.ISOFolderBox.Size = new System.Drawing.Size(159, 22);
+            this.ISOFolderBox.TabIndex = 12;
+            // 
+            // PKGListBox
+            // 
+            this.PKGListBox.Location = new System.Drawing.Point(22, 234);
+            this.PKGListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.PKGListBox.Name = "PKGListBox";
+            this.PKGListBox.Size = new System.Drawing.Size(159, 22);
+            this.PKGListBox.TabIndex = 11;
+            // 
+            // PKGListLabel
+            // 
+            this.PKGListLabel.AutoSize = true;
+            this.PKGListLabel.Location = new System.Drawing.Point(18, 214);
+            this.PKGListLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PKGListLabel.Name = "PKGListLabel";
+            this.PKGListLabel.Size = new System.Drawing.Size(95, 16);
+            this.PKGListLabel.TabIndex = 10;
+            this.PKGListLabel.Text = "Packages List:";
+            // 
+            // ISOFolderLabel
+            // 
+            this.ISOFolderLabel.AutoSize = true;
+            this.ISOFolderLabel.Location = new System.Drawing.Point(17, 168);
+            this.ISOFolderLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ISOFolderLabel.Name = "ISOFolderLabel";
+            this.ISOFolderLabel.Size = new System.Drawing.Size(74, 16);
+            this.ISOFolderLabel.TabIndex = 9;
+            this.ISOFolderLabel.Text = "ISO Folder:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(422, 554);
+            this.ClientSize = new System.Drawing.Size(696, 554);
+            this.Controls.Add(this.ChocoSpreadButton);
+            this.Controls.Add(this.PKGListButton);
+            this.Controls.Add(this.ISOFolderButton);
+            this.Controls.Add(this.ISOFolderBox);
+            this.Controls.Add(this.PKGListBox);
+            this.Controls.Add(this.PKGListLabel);
+            this.Controls.Add(this.ISOFolderLabel);
             this.Controls.Add(this.OutputLabel);
             this.Controls.Add(this.OutputBox);
             this.Controls.Add(this.ExtractISOButton);
@@ -147,11 +228,10 @@
             this.Controls.Add(this.ISOPathLabel);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "ChocolateySpreader";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,6 +248,13 @@
         private System.Windows.Forms.Button ExtractISOButton;
         private System.Windows.Forms.RichTextBox OutputBox;
         private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button ChocoSpreadButton;
+        private System.Windows.Forms.Button PKGListButton;
+        private System.Windows.Forms.Button ISOFolderButton;
+        private System.Windows.Forms.TextBox ISOFolderBox;
+        private System.Windows.Forms.TextBox PKGListBox;
+        private System.Windows.Forms.Label PKGListLabel;
+        private System.Windows.Forms.Label ISOFolderLabel;
     }
 }
 
