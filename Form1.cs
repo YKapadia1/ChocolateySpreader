1debc1f2d547f2110e83b4cf24753b339f7a4c40
YKapadia <YKapadia@uclan.ac.uk>
2022-11-10 15:36:18 +0000

    Moved program strings to a different file for better code management

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Text;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.WindowsAPICodePack.Dialogs;
 
 
 namespace ChocolateySpreader
@@ -24,62 +16,11 @@ namespace ChocolateySpreader
             InitializeComponent();
         }
 
-        
-        //Strings for the various dialogs presented to the user.
-        const string DEFAULT_7ZIP_LOCATION = "C:\\Program Files\\7-Zip\\7z.exe";
-        
-        const string ISO_SELECT_WINDOW_TITLE = "Select Windows ISO";
-        const string ISO_SELECT_WINDOW_DIRECTORY = "C:\\";
-        const string ISO_SELECT_WINDOW_FILTER = "ISO Files (*.iso)|*.iso|All Files (*.*)|*.*";
-        
-        const string OUTPUT_FOLDER_SELECT_TITLE = "Select folder to extract ISO to:";
-
-        const string ZIP_SELECT_WINDOW_TITLE = "Locate 7z.exe";
-        const string ZIP_SELECT_WINDOW_DIRECTORY = "C:\\Program Files";
-        const string ZIP_SELECT_WINDOW_FILTER = "7-Zip|7z.exe";
-
-
-        //Strings containing the various messages that may be displayed to the user.
-        //Strings related to selecting an ISO and an output folder.
-        const string ERR_NO_ISO_SPECIFIED_TITLE = "No ISO file";
-        const string ERR_NO_ISO_SPECIFIED = "You have not specified an ISO file to extract.";
-        const string ERR_NO_ISO_FOLDER_SPECIFIED_TITLE = "No ISO folder";
-        const string ERR_NO_ISO_FOLDER_SPECIFIED = "You have not specified the folder where the Windows ISO files are located.";
-        const string ERR_NO_OUTPUT_SPECIFIED_TITLE = "No output folder";
-        const string ERR_NO_OUTPUT_SPECIFIED = "You have not specified an output folder to extract to.";
-        const string ERR_ISO_EXTRACT_FATAL = "A fatal error occured when extracting the ISO.";
-        const string ERR_ISO_EXTRACT_CLI = "A command line error occured when extracting the ISO.";
-        const string ERR_ISO_EXTRACT_NO_MEMORY = "There is not enough free memory available to extract the ISO.";
-        const string ERR_ISO_EXTRACT_USER_ABORT = "The user cancelled the operation.";
-        
-        const string WARN_SPECIFY_ISO_BEFORE_OUTPUT = "Please specify an ISO before specifiying the output folder.";
-        const string WARN_SPECIFY_ISO_BEFORE_OUTPUT_TITLE = "No ISO file";
-        const string WARN_OUTPUT_FOLDER_NOT_EMPTY = "There appear to be files/folders inside the output folder you specified. " +
-            "Would you like to extract to this folder anyway?";
-        
-        //Strings related to the status of the extraction process.
-        const string INFO_ISO_EXTRACT_SUCCESS = "ISO extracted successfully!";
-        const string INFO_ISO_EXTRACT_WARNING = "ISO extracted with warnings. Please check for any corrupt/missing files.";
-
-        //Strings related to locating 7-Zip.
-        const string ERR_INVALID_7Z_EXE = "Invalid 7z.exe supplied!";
-        const string INFO_7Z_INSTALL_TIP = "If you do not have 7-Zip installed, you can get it from 7-zip.org.";
-
-        //Strings related to selecting a folder with the ISO files and a packages list file.
-        const string ERR_INVALID_ISO_FOLDER_SPECIFIED_TITLE = "Invalid Windows ISO folder";
-        const string ERR_INVALID_ISO_FOLDER_SPECIFIED = "Could not find install.esd/.wim/.swm. Please check that the folder contains valid Windows ISO files.";
-        const string ERR_NO_PACKAGE_LIST_SPECIFIED_TITLE = "No packages.config file";
-        const string ERR_NO_PACKAGE_LIST_SPECIFIED = "You have not specified the location to your Chocolatey package list. You can create one by going to https://community.chocolatey.org/packages.\n " +
-            "If Chocolatey is installed on this machine, type \"choco export\" followed by the location you want the file to be placed.";
-        const string ISO_FOLDER_SELECT_TITLE = "Select folder with Windows ISO files:";
-
-
-
         void Checkfor7Z(ref string SevenZipLocation)
         {
             //Check if 7-Zip is installed. This should work on both 32 and 64 bit systems.
             //This will fail if the user has specified an alternate install location!
-            bool SevenZipInstalled = File.Exists(DEFAULT_7ZIP_LOCATION);
+            bool SevenZipInstalled = File.Exists(ProgramStrings.DEFAULT_7ZIP_LOCATION);
             //If 7-Zip is not installed in the usual location...
             if (!SevenZipInstalled)
             {
@@ -88,9 +29,10 @@ namespace ChocolateySpreader
                 {
                     using (OpenFileDialog ZipSelectDialog = new OpenFileDialog())
                     {
-                        ZipSelectDialog.Title = ZIP_SELECT_WINDOW_TITLE; //Set the title of the window.
-                        ZipSelectDialog.InitialDirectory = ZIP_SELECT_WINDOW_DIRECTORY; //Set the initial directory of the selection window.
-                        ZipSelectDialog.Filter = ZIP_SELECT_WINDOW_FILTER; //Set the filter so that it only shows exe files called 7z.
+                        //Set the title, filter, and directory of the open file dialog.
+                        ZipSelectDialog.Title = ProgramStrings.ZIP_SELECT_WINDOW_TITLE;
+                        ZipSelectDialog.InitialDirectory = ProgramStrings.ZIP_SELECT_WINDOW_DIRECTORY; 
+                        ZipSelectDialog.Filter = ProgramStrings.ZIP_SELECT_WINDOW_FILTER;
                         ZipSelectDialog.FilterIndex = 1; //Set the selected filter to be 1.
                         ZipSelectDialog.Multiselect = false; //Disallow the user from selecting multiple files.
                                                              //Show the dialog, and if the user has supplied a file...
@@ -99,7 +41,7 @@ namespace ChocolateySpreader
                             //Check that the user has supplied a valid 7z.exe.
                             if (ZipSelectDialog.FileName.EndsWith("7z.exe") != true)
                             {
-                                MessageBox.Show(ERR_INVALID_7Z_EXE, this.Text,
+                                MessageBox.Show(ProgramStrings.ERR_INVALID_7Z_EXE, this.Text,
                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
                             }
                             else
@@ -112,7 +54,7 @@ namespace ChocolateySpreader
                         else //If the user has not supplied a file...
                         {
                             LookingFor7Z = false; //Assume they do not have 7-Zip installed.
-                            MessageBox.Show(INFO_7Z_INSTALL_TIP, this.Text,
+                            MessageBox.Show(ProgramStrings.INFO_7Z_INSTALL_TIP, this.Text,
                             MessageBoxButtons.OK, MessageBoxIcon.Information);
                             //Open the 7-Zip website.
                             Process.Start("https://www.7-zip.org/");
@@ -128,15 +70,13 @@ namespace ChocolateySpreader
             }
         }
 
-
-
-
-
         private void OutputLog(object sendingProcess, DataReceivedEventArgs e)
         {
             if (e.Data != null)
             {
                 BeginInvoke(new MethodInvoker(() => { OutputBox.AppendText(e.Data + Environment.NewLine);}));
+                //Invoke the UI thread and update the text box. It must be done this way to ensure asynchronous operation.
+                //If this was done synchronously, the UI would freeze.
             }
         }
 
@@ -144,9 +84,11 @@ namespace ChocolateySpreader
         {
             ExtractISOButton.Enabled = false;
             ISOSelectButton.Enabled = false;
+            ISOFolderButton.Enabled = false;
+            PKGListButton.Enabled = false;
             OutputFolderSelectButton.Enabled = false;
             Process ExtractISO = new Process(); //Create a new process that we will start.
-                                                //Set the file path to where 7-Zip is usually located.
+                                                //Set the file path to where 7-Zip has been located.
             ExtractISO.StartInfo.FileName = SevenZipLocation;
             ExtractISO.StartInfo.Arguments = " -o" + FolderPathBox.Text + " -aoa -bsp1 x " + ISOPathBox.Text; 
             //Create the arguments necessary.
@@ -156,7 +98,9 @@ namespace ChocolateySpreader
             ExtractISO.StartInfo.CreateNoWindow = true;
             ExtractISO.StartInfo.RedirectStandardOutput = true;
             ExtractISO.StartInfo.UseShellExecute = false;
-            ExtractISO.OutputDataReceived += new DataReceivedEventHandler(OutputLog);
+            
+            //Send any output to the event handler responsible for updating the text box.
+            ExtractISO.OutputDataReceived += new DataReceivedEventHandler(OutputLog); 
             ExtractISO.Start(); //Start the process.
             ExtractISO.BeginOutputReadLine(); //Begin asynchronously reading the output.
             while (!ExtractISO.HasExited) //If the process has not yet exited...
@@ -166,27 +110,27 @@ namespace ChocolateySpreader
             switch (ExtractISO.ExitCode) //Check the exit code.
             {
                 case 0: //If there was no error...
-                    MessageBox.Show(INFO_ISO_EXTRACT_SUCCESS, this.Text,
+                    MessageBox.Show(ProgramStrings.INFO_ISO_EXTRACT_SUCCESS, this.Text,
                         MessageBoxButtons.OK, MessageBoxIcon.Information) ;
                     break;
                 case 1: //If there were non-fatal errors/warnings...
-                    MessageBox.Show(INFO_ISO_EXTRACT_WARNING, this.Text,
+                    MessageBox.Show(ProgramStrings.INFO_ISO_EXTRACT_WARNING, this.Text,
                         MessageBoxButtons.OK, MessageBoxIcon.Information);
                     break;
                 case 2: //If there was a fatal error...
-                    MessageBox.Show(ERR_ISO_EXTRACT_FATAL, this.Text,
+                    MessageBox.Show(ProgramStrings.ERR_ISO_EXTRACT_FATAL, this.Text,
                        MessageBoxButtons.OK, MessageBoxIcon.Error);
                     break;
                 case 7: //If there was a command line error.
-                    MessageBox.Show(ERR_ISO_EXTRACT_CLI, this.Text,
+                    MessageBox.Show(ProgramStrings.ERR_ISO_EXTRACT_CLI, this.Text,
                        MessageBoxButtons.OK, MessageBoxIcon.Error);
                     break;
                 case 8: //If we have ran out of memory...
-                    MessageBox.Show(ERR_ISO_EXTRACT_NO_MEMORY, this.Text,
+                    MessageBox.Show(ProgramStrings.ERR_ISO_EXTRACT_NO_MEMORY, this.Text,
                        MessageBoxButtons.OK, MessageBoxIcon.Error);
                     break;
                 case -1073741510: //If the user closed the window before it was completed or pressed CTRL+C...
-                    MessageBox.Show(ERR_ISO_EXTRACT_USER_ABORT, this.Text,
+                    MessageBox.Show(ProgramStrings.ERR_ISO_EXTRACT_USER_ABORT, this.Text,
                        MessageBoxButtons.OK, MessageBoxIcon.Error);
                     break;
             }
@@ -219,11 +163,30 @@ namespace ChocolateySpreader
             
         }
 
+        string CreateOpenFolderDialog(string name)
+        {
+            //Set up the file dialog by specifying its a folder picker, and set the initial directory.
+            using (CommonOpenFileDialog FolderSelectDialog = new CommonOpenFileDialog())
+            {
+                FolderSelectDialog.IsFolderPicker = true;
+                FolderSelectDialog.Title = name;
+
+                //Show the dialog, and if the user has supplied a folder...
+                if (FolderSelectDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    //Return the folder path.
+                    return FolderSelectDialog.FileName;
+                }
+            }
+            return null; //Return null if the user pressed Cancel.
+        }
+
 
         //When the user has clicked the button to browse for an ISO file...
         private void ISOSelectButton_Click(object sender, EventArgs e) 
         {
-            ISOPathBox.Text = CreateOpenFileDialog(ISO_SELECT_WINDOW_TITLE, ISO_SELECT_WINDOW_DIRECTORY,ISO_SELECT_WINDOW_FILTER);
+            ISOPathBox.Text = CreateOpenFileDialog(ProgramStrings.ISO_SELECT_WINDOW_TITLE, ProgramStrings.ISO_SELECT_WINDOW_DIRECTORY
+                ,ProgramStrings.ISO_SELECT_WINDOW_FILTER);
         }
 
         //When the user has clicked the button to browse for an ISO file...
@@ -232,47 +195,35 @@ namespace ChocolateySpreader
             //If the user has not yet specified an ISO file...
             if (ISOPathBox.Text == "")
             {
-                MessageBox.Show(WARN_SPECIFY_ISO_BEFORE_OUTPUT, WARN_SPECIFY_ISO_BEFORE_OUTPUT_TITLE,
+                MessageBox.Show(ProgramStrings.WARN_SPECIFY_ISO_BEFORE_OUTPUT, ProgramStrings.WARN_SPECIFY_ISO_BEFORE_OUTPUT_TITLE,
                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
             else
             {
-                //Set up the file dialog by specifying its a folder picker, and set the initial directory.
-                using (CommonOpenFileDialog OutputFolderSelectDialog = new CommonOpenFileDialog())
-                {
-                    OutputFolderSelectDialog.IsFolderPicker = true;
-                    OutputFolderSelectDialog.Title = OUTPUT_FOLDER_SELECT_TITLE;
-
-                    //Show the dialog, and if the user has supplied a folder...
-                    if (OutputFolderSelectDialog.ShowDialog() == CommonFileDialogResult.Ok)
-                    {
-                        //Get the folder path, and put it into the text box.
-                        FolderPathBox.Text = OutputFolderSelectDialog.FileName;
-                    }
-                }
+                FolderPathBox.Text = CreateOpenFolderDialog(ProgramStrings.OUTPUT_FOLDER_SELECT_TITLE);
             }
         }
 
         //When the user clicks the button to extract the ISO...
         private void ExtractISOButton_Click(object sender, EventArgs e)
         {
-            string SevenZipLocation = DEFAULT_7ZIP_LOCATION;
+            string SevenZipLocation = ProgramStrings.DEFAULT_7ZIP_LOCATION;
             //If the user has not selected an ISO...
             if (ISOPathBox.Text == "")
             {
-                MessageBox.Show(ERR_NO_ISO_SPECIFIED, ERR_NO_ISO_SPECIFIED_TITLE,
+                MessageBox.Show(ProgramStrings.ERR_NO_ISO_SPECIFIED, ProgramStrings.ERR_NO_ISO_SPECIFIED_TITLE,
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             //If the user has not selected an output folder...
             else if (FolderPathBox.Text == "")
             {
-                MessageBox.Show(ERR_NO_OUTPUT_SPECIFIED, ERR_NO_OUTPUT_SPECIFIED_TITLE,
+                MessageBox.Show(ProgramStrings.ERR_NO_OUTPUT_SPECIFIED, ProgramStrings.ERR_NO_OUTPUT_SPECIFIED_TITLE,
                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             //INFO: This is some janky ass shit right here... Try to make this neater!
             else if (Directory.GetFileSystemEntries(FolderPathBox.Text).Length != 0)
             {
-                switch (MessageBox.Show(WARN_OUTPUT_FOLDER_NOT_EMPTY, this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Warning))
+                switch (MessageBox.Show(ProgramStrings.WARN_OUTPUT_FOLDER_NOT_EMPTY, this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Warning))
                 {
                     case DialogResult.Yes:
                         Checkfor7Z(ref SevenZipLocation);
@@ -290,32 +241,26 @@ namespace ChocolateySpreader
         {
             if (ISOFolderBox.Text == "")
             {
-                MessageBox.Show(ERR_NO_ISO_FOLDER_SPECIFIED, ERR_NO_ISO_FOLDER_SPECIFIED_TITLE,
+                MessageBox.Show(ProgramStrings.ERR_NO_ISO_FOLDER_SPECIFIED, ProgramStrings.ERR_NO_ISO_FOLDER_SPECIFIED_TITLE,
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             //If the user has not selected an output folder...
             else if (PKGListBox.Text == "")
             {
-                MessageBox.Show(ERR_NO_PACKAGE_LIST_SPECIFIED, ERR_NO_PACKAGE_LIST_SPECIFIED_TITLE,
+                MessageBox.Show(ProgramStrings.ERR_NO_PACKAGE_LIST_SPECIFIED, ProgramStrings.ERR_NO_PACKAGE_LIST_SPECIFIED_TITLE,
                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void ISOFolderButton_Click(object sender, EventArgs e)
         {
-            using (CommonOpenFileDialog ISOFolderSelectDialog = new CommonOpenFileDialog())
-            {
-                ISOFolderSelectDialog.IsFolderPicker = true;
-                ISOFolderSelectDialog.Title = ISO_FOLDER_SELECT_TITLE;
-
-                //Show the dialog, and if the user has supplied a folder...
-                if (ISOFolderSelectDialog.ShowDialog() == CommonFileDialogResult.Ok)
-                {
-                    //Get the folder path, and put it into the text box.
-                    ISOFolderBox.Text = ISOFolderSelectDialog.FileName;
-                }
-            }
+           ISOFolderBox.Text = CreateOpenFolderDialog(ProgramStrings.ISO_FOLDER_SELECT_TITLE);
         }
 
+        private void PKGListButton_Click(object sender, EventArgs e)
+        {
+            PKGListBox.Text = CreateOpenFileDialog(ProgramStrings.ISO_FOLDER_SELECT_TITLE, 
+                ProgramStrings.ISO_FOLDER_SELECT_WINDOW_DIRECTORY, ProgramStrings.ISO_FOLDER_SELECT_WINDOW_FILTER);
+        }
     }
 }
---
+++ ProgramStrings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateySpreader
+{
+    public class ProgramStrings
+    {
+        //Strings for the various dialogs presented to the user.
+        public const string DEFAULT_7ZIP_LOCATION = "C:\\Program Files\\7-Zip\\7z.exe";
+
+        public const string ISO_SELECT_WINDOW_TITLE = "Select Windows ISO";
+        public const string ISO_SELECT_WINDOW_DIRECTORY = "C:\\";
+        public const string ISO_SELECT_WINDOW_FILTER = "ISO Files (*.iso)|*.iso|All Files (*.*)|*.*";
+
+        public const string ISO_FOLDER_SELECT_WINDOW_TITLE = "Select packages.config";
+        public const string ISO_FOLDER_SELECT_WINDOW_DIRECTORY = "C:\\";
+        public const string ISO_FOLDER_SELECT_WINDOW_FILTER = "Package List (packages.config)|packages.config|All Files(*.*)|*.*";
+
+        public const string OUTPUT_FOLDER_SELECT_TITLE = "Select folder to extract ISO to:";
+
+        public const string ZIP_SELECT_WINDOW_TITLE = "Locate 7z.exe";
+        public const string ZIP_SELECT_WINDOW_DIRECTORY = "C:\\Program Files";
+        public const string ZIP_SELECT_WINDOW_FILTER = "7-Zip|7z.exe";
+
+
+        //Strings containing the various messages that may be displayed to the user.
+        //Strings related to selecting an ISO and an output folder.
+        public const string ERR_NO_ISO_SPECIFIED_TITLE = "No ISO file";
+        public const string ERR_NO_ISO_SPECIFIED = "You have not specified an ISO file to extract.";
+        public const string ERR_NO_ISO_FOLDER_SPECIFIED_TITLE = "No ISO folder";
+        public const string ERR_NO_ISO_FOLDER_SPECIFIED = "You have not specified the folder where the Windows ISO files are located.";
+        public const string ERR_NO_OUTPUT_SPECIFIED_TITLE = "No output folder";
+        public const string ERR_NO_OUTPUT_SPECIFIED = "You have not specified an output folder to extract to.";
+        public const string ERR_ISO_EXTRACT_FATAL = "A fatal error occured when extracting the ISO.";
+        public const string ERR_ISO_EXTRACT_CLI = "A command line error occured when extracting the ISO.";
+        public const string ERR_ISO_EXTRACT_NO_MEMORY = "There is not enough free memory available to extract the ISO.";
+        public const string ERR_ISO_EXTRACT_USER_ABORT = "The user cancelled the operation.";
+
+        public const string WARN_SPECIFY_ISO_BEFORE_OUTPUT = "Please specify an ISO before specifiying the output folder.";
+        public const string WARN_SPECIFY_ISO_BEFORE_OUTPUT_TITLE = "No ISO file";
+        public const string WARN_OUTPUT_FOLDER_NOT_EMPTY = "There appear to be files/folders inside the output folder you specified. " +
+            "Would you like to extract to this folder anyway?";
+
+        //Strings related to the status of the extraction process.
+        public const string INFO_ISO_EXTRACT_SUCCESS = "ISO extracted successfully!";
+        public const string INFO_ISO_EXTRACT_WARNING = "ISO extracted with warnings. Please check for any corrupt/missing files.";
+
+        //Strings related to locating 7-Zip.
+        public const string ERR_INVALID_7Z_EXE = "Invalid 7z.exe supplied!";
+        public const string INFO_7Z_INSTALL_TIP = "If you do not have 7-Zip installed, you can get it from 7-zip.org.";
+
+        //Strings related to selecting a folder with the ISO files and a packages list file.
+        public const string ERR_INVALID_ISO_FOLDER_SPECIFIED_TITLE = "Invalid Windows ISO folder";
+        public const string ERR_INVALID_ISO_FOLDER_SPECIFIED = "Could not find install.esd/.wim/.swm. Please check that the folder contains valid Windows ISO files.";
+        public const string ERR_NO_PACKAGE_LIST_SPECIFIED_TITLE = "No packages.config file";
+        public const string ERR_NO_PACKAGE_LIST_SPECIFIED = "You have not specified the location to your Chocolatey package list. You can create one by going to https://community.chocolatey.org/packages.\n " +
+            "If Chocolatey is installed on this machine, type \"choco export\" followed by the location you want the file to be placed.";
+        public const string ISO_FOLDER_SELECT_TITLE = "Select folder with Windows ISO files:";
+    }
+}
