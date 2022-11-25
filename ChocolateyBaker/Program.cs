452b12c2e0d614bda9b87bc7a4b966438c0a38e4
YKapadia <YKapadia@uclan.ac.uk>
2022-11-25 16:42:54 +0000

    Finalised post-install scripts

--- ChocolateyBaker/Program.cs
+++ ChocolateyBaker/Program.cs
@@ -6,15 +6,49 @@ namespace ChocolateyBaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            if (args.Length == 0)
+            string InstallDrive = null;
+            Console.WriteLine("Installing Chocolatey and your packages now. You should see the icons appear on your desktop...\n");
+            while (InstallDrive == null)
             {
-                Console.WriteLine("This program requires arguments in order to function properly.");
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-                Environment.Exit(2);
+                DriveInfo[] currentDrives = DriveInfo.GetDrives();
+                for (int i = 0; i < currentDrives.Length; i++)
+                {
+                    if (File.Exists(currentDrives[i].Name + "\\sources\\install.esd")
+                        || File.Exists(currentDrives[i].Name + "\\sources\\install.wim")
+                        || File.Exists(currentDrives[i].Name + "\\sources\\install.swm"))
+                    {
+                        InstallDrive = currentDrives[i].Name;
+                    }
+
+                }
+                if (InstallDrive == null)
+                {
+                    Console.WriteLine("Could not find the drive used to install Windows!");
+                    Console.WriteLine("Please insert the drive that was used to install Windows and press any key to continue...\n");
+                    Console.ReadKey();
+                }
             }
+            Process instChoco = new Process();
+            instChoco.StartInfo.FileName = "powershell.exe";
+            instChoco.StartInfo.Arguments = "-NoProfile -Inputformat None -ExecutionPolicy Bypass -Command " +
+            "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; " +
+            @"iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))";
+            instChoco.StartInfo.CreateNoWindow = false;
+            instChoco.StartInfo.UseShellExecute = false;
+            instChoco.Start();
+            instChoco.WaitForExit();
+
+
+            Process instPackages = new Process();
+            instPackages.StartInfo.FileName = "powershell.exe";
+            instPackages.StartInfo.Arguments = "choco install " + InstallDrive + @"setup\packages.config -y";
+            instPackages.StartInfo.RedirectStandardOutput = false;
+            instPackages.StartInfo.CreateNoWindow = false;
+            instPackages.StartInfo.UseShellExecute = false;
+            instPackages.Start();
+            instPackages.WaitForExit();
         }
     }
 
